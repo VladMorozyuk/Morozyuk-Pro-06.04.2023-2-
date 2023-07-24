32f1c16c557613c9d807adac0f22c459f8e30335
VladMorozyuk <moroziukvladyslav@gmail.com>
2023-07-24 11:41:42 +0300

    next logic

--- index.js
+++ index.js
@@ -44,3 +44,47 @@ function resetProgram() {
 }
 
 resetProgram();
+
+function buyProduct() {
+    var productDetails = document.getElementById("productDetails");
+    productDetails.style.display = "none";
+
+    var orderForm = document.getElementById("orderForm");
+    orderForm.style.display = "block";
+}
+
+function submitOrder() {
+    var fullName = document.getElementById("fullName").value;
+    var city = document.getElementById("city").value;
+    var postOffice = document.getElementById("postOffice").value;
+    var paymentMethod = document.querySelector('input[name="paymentMethod"]:checked');
+    var quantity = document.getElementById("quantity").value;
+    var comment = document.getElementById("comment").value;
+
+
+    if (!fullName || !city || !postOffice || !paymentMethod || !quantity) {
+        alert("Будь ласка, заповніть всі обов'язкові поля.");
+        return false;
+    }
+
+
+    var orderDetails = document.getElementById("orderDetails");
+    orderDetails.innerHTML = `
+        <h3>Інформація про замовлення:</h3>
+        <p>Товар: ${productName.textContent}</p>
+        <p>ПІБ покупця: ${fullName}</p>
+        <p>Місто: ${city}</p>
+        <p>Склад Нової пошти: ${postOffice}</p>
+        <p>Спосіб оплати: ${paymentMethod.value}</p>
+        <p>Кількість: ${quantity}</p>
+        <p>Коментар: ${comment}</p>
+    `;
+
+    var orderForm = document.getElementById("orderForm");
+    orderForm.style.display = "none";
+
+    var orderDetails = document.getElementById("orderDetails");
+    orderDetails.style.display = "block";
+
+    return false;
+}
